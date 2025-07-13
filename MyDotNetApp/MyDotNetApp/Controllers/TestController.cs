cd23b186ef8d92d99710e133d626e24fab8dde15
DoHung0411 <dodinhhungHaui@gmail.com>
2025-07-13 10:25:55 +0700

    chore: Trigger CI/CD to re-test after fixing permissions

--- MyDotNetApp/MyDotNetApp/Controllers/TestController.cs
+++ MyDotNetApp/MyDotNetApp/Controllers/TestController.cs
@@ -9,7 +9,7 @@ namespace MyDotNetApp.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(new { message = "Hello from Test API! testted" });
+            return Ok(new { message = "Hello from Test API abc! testted" });
         }
     }
 } 
