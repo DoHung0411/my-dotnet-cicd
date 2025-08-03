8743e0fa1bc64f1d6bc5fbe6fce3d390f08bb946
DoHung0411 <dodinhhungHaui@gmail.com>
2025-08-03 16:33:15 +0700

    Re-trigger CI/CD pipeline

--- MyDotNetApp/MyDotNetApp/Program.cs
+++ MyDotNetApp/MyDotNetApp/Program.cs
@@ -15,6 +15,8 @@ namespace MyDotNetApp
         {
             CreateHostBuilder(args).Build().Run();
         }
+
+        
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
